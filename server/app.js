7f3a74afa29a940fbce0ed11786bfdafcda3ef42
Erin Shaw <erin.kimberly.shaw@gmail.com>
2017-11-29 12:33:23 -0500

    made backend more modular

--- server/app.js
+++ server/app.js
@@ -12,57 +12,9 @@ app.use(morgan('dev'))
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'public')))
 
-
-//&pagetoken=
-// get nearby restaurants
-app.get('/places/lat/:lat/lng/:lng', (req, res, next) => {
-  //if there is a query that means it's the next page token
-  const placesUrl =
-  req.query.token ? `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.params.lat},${req.params.lng}&radius=500&types=food&key=${process.env.GOOGLE_API_KEY}&pagetoken=${req.query.token}`
-  :
-  `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.params.lat},${req.params.lng}&radius=500&types=food&key=${process.env.GOOGLE_API_KEY}`
-  console.log(placesUrl)
-
-  if (req.query.token) {
-    setTimeout(() => {
-      axios.get(placesUrl)
-      .then(res => res.data)
-      .then(data => {
-        console.log('DATAAAAA', data)
-        res.status(200).send(data);
-      })
-    }, 2000);
-  }
-  else {
-    axios.get(placesUrl)
-    .then(res => res.data)
-    .then(data => {
-      console.log('DATAAAAA', data)
-      res.status(200).send(data);
-    })
-  }
-})
-
-// get image for restaurant
-app.get('/places/img/:photoReference', (req, res, next) => {
-  const photoUrl = makeGooglePlacesPhotoURL(req.params.photoReference)
-  res.json(photoUrl)
-})
-
-// get map for restaurant
-app.get('/places/map/:placeId', (req, res, next) => {
-  const placeUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_API_KEY}&q=place_id:${req.params.placeId}`
-  res.json(placeUrl)
-})
+app.use('/places', require('./places'))
 
 app.get('/*', (_, res) => res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html')))
-
-function makeGooglePlacesPhotoURL(photoReference) {
-  var baseURL = 'https://maps.googleapis.com/maps/api/place/photo?';
-  var maxHeight = 200;
-  var fullURL = baseURL + 'key=' + process.env.GOOGLE_API_KEY + '&' + 'maxheight=' + maxHeight + '&' + 'photoreference=' + photoReference;
-  return fullURL;
-}
 
 module.exports = app;
 
---
+++ server/places.js
@@ -0,0 +1,51 @@
+const router = require('express').Router()
+const axios = require('axios')
+
+// get nearby restaurants
+router.get('/lat/:lat/lng/:lng', (req, res, next) => {
+
+  let placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.params.lat},${req.params.lng}&radius=500&types=food&key=${process.env.GOOGLE_API_KEY}`
+
+  //if there is a query that means it's the next page token
+  placesUrl = req.query.token ? `${placesUrl}&pagetoken=${req.query.token}` : placesUrl
+
+  if (req.query.token) {
+    setTimeout(() => {
+      axios.get(placesUrl)
+      .then(res => res.data)
+      .then(data => {
+        res.status(200).send(data);
+      })
+    }, 2000);
+  }
+  else {
+    axios.get(placesUrl)
+    .then(res => res.data)
+    .then(data => {
+      res.status(200).send(data);
+    })
+  }
+})
+
+// get image for restaurant
+router.get('/img/:photoReference', (req, res, next) => {
+  const photoUrl = makeGooglePlacesPhotoURL(req.params.photoReference)
+  res.json(photoUrl)
+})
+
+// get map for restaurant
+router.get('/map/:placeId', (req, res, next) => {
+  const placeUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_API_KEY}&q=place_id:${req.params.placeId}`
+  res.json(placeUrl)
+})
+
+function makeGooglePlacesPhotoURL(photoReference) {
+  var baseURL = 'https://maps.googleapis.com/maps/api/place/photo?';
+  var maxHeight = 200;
+  var fullURL = baseURL + 'key=' + process.env.GOOGLE_API_KEY + '&' + 'maxheight=' + maxHeight + '&' + 'photoreference=' + photoReference;
+  return fullURL;
+}
+
+module.exports = router
+
+// https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyDArX30BmlDv_D0LOJ1242aogy3G5r8Y70
