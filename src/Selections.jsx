afaa35f7f824b7ad8d6902eee32c54e5dbc055c8
Erin Shaw <erin.kimberly.shaw@gmail.com>
2018-03-23 11:08:44 -0400

    ok got list back with special result if they haven't swiped anything

--- src/Selections.jsx
+++ src/Selections.jsx
@@ -3,9 +3,10 @@ import { NavLink } from 'react-router-dom'
 import Restaurant from './Restaurant'
 import { connect } from 'react-redux'
 import store from './store'
-import { List } from 'material-ui/List'
+import { List, ListItem } from 'material-ui/List'
 import Divider from 'material-ui/Divider'
 import Subheader from 'material-ui/Subheader'
+import Avatar from 'material-ui/Avatar'
 
 
 class Selections extends Component {
@@ -17,22 +18,26 @@ class Selections extends Component {
         <div className="container">
           <NavLink activeClassName="active" to={`/`} >
             <img src="/img/salad.svg"
-            className="shiver"
-            id="getCards" />
+              className="shiver"
+              id="getCards" />
           </NavLink>
           <div id="chatbox">
-            {!selections.length
-              ? <h1 className="middle">You haven't swiped anything yet!</h1> :
-              <List>
-                <Subheader>{`Messages: ${selections.length}`}</Subheader>
-                {selections.map((image, i) => {
+            <List>
+              <Subheader>{`Messages: ${selections.length}`}</Subheader>
+              {!selections.length
+                ? <ListItem
+                leftAvatar={<Avatar src={"/img/cutlery.svg"} />}
+                primaryText={`No matches :(`}
+                secondaryText={<p>{`Hit the salad bowl and get swiping`}</p>}
+                secondaryTextLines={2} />
+                :
+                selections.map((image, i) => {
                   const placeId = placesMap[image.photo_reference]
                   const place = places.find(place => place.place_id === placeId)
                   return <Restaurant key={i} place={place} photoReference={image.photo_reference} />
-                }
-                )}
-              </List>
-            }
+                })
+              }
+            </List>
           </div>
         </div>
       </div>
