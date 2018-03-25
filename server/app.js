026cda2b185d3951c78e96ca5100ce1b5dfb0aef
Erin Shaw <erin.kimberly.shaw@gmail.com>
2018-03-25 17:09:16 -0400

    change server file pre merge

--- server/app.js
+++ server/app.js
@@ -3,19 +3,25 @@ const morgan = require('morgan')
 const path = require('path')
 const app = express()
 const axios = require('axios')
+const bodyParser = require('body-parser')
 
 // Setup logger
 app.use(morgan('dev'))
 
+// For handling my Clarifai batch requests
+app.use(bodyParser.json())
+   .use(bodyParser.urlencoded({ extended: true }))
+
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'public')))
 
-app.use('/places', require('./places'))
+app.use('/api/places', require('./api/places'))
+
+app.use('/api/clarifai', require('./api/clarifai'))
 
 app.get('/*', (_, res) => res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html')))
 
 module.exports = app;
-
 
 
 
